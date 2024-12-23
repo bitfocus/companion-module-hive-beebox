c91565394489dcbe6e37853b1f0966b00172a238
Nigel Sadler <nigel.sadler@green-hippo.com>
2024-12-23 11:05:35 +0000

    Started on Variables

--- variables.js
+++ variables.js
@@ -1,7 +1,15 @@
 module.exports = function (self) {
 	self.setVariableDefinitions([
-		{ variableId: 'softver', name: 'Software Version' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
+		{ variableId: 'playlistenabled', name: 'Playlist Enabled' },
+		{ variableId: 'timelineenabled', name: 'Timeline Enabled' },
+		{ variableId: 'scheduleenabled', name: 'Schedule Enabled' },
+		{ variableId: 'l1timecodeenabled', name: 'Timecode Layer 1 Enabled' },
+		{ variableId: 'l2timecodeenabled', name: 'Timecode Layer 2 Enabled' },
+		{ variableId: 'screenberryenabled', name: 'Screenberry Mapping Enabled' },
+		{ variableId: 'visioenabled', name: 'Visio Mapping Enabled' },
+		{ variableId: 'screenberryconfigmode', name: 'Screenberry Config Mode Enabled' },
+		{ variableId: 'viosoconfigmode', name: 'Vioso Config Mode Enabled' },
+		{ variableId: 'playlistrowcount', name: 'Playlist Row Count' },
+		{ variableId: 'playlistcurrentrow', name: 'Playlist Current Row' },
 	])
 }
